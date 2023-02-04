2d37ef62a66edea1e5ac3b6963e6f97d2a4cb80d
romiramadani21 <romiramaadani@gmail.com>
2023-02-04 19:27:47 +0700

    Add Conditional in C#

---
+++ csharp/Conditional.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Conditional
+    {
+       /* static void Main(string[] args)
+        {
+            *//* int x = 3;
+             int y = 5;
+             if (x > y)
+             {
+                 Console.WriteLine("{0} lebih besar dari {1}", x, y);
+             }
+             else
+             {
+                 Console.WriteLine("{0} lebih kecil dari {1}", x, y);
+             }*/
+
+            /* int nilai = 85;
+             if (nilai >= 50)
+             {
+                 Console.WriteLine("Anda Berhasil");
+                 if (nilai >= 85)
+                 {
+                     Console.WriteLine("Perfect !");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("Anda Gagal");
+             }*//*
+
+            int umur = 20;
+            if (umur > 14)
+            {
+                if (umur > 18)
+                {
+                    Console.WriteLine("Dewasa");
+                }
+                else
+                {
+                    Console.WriteLine("Remaja");
+                }
+            }
+            else
+            {
+                if (umur > 5)
+                {
+                    Console.WriteLine("Anak - anak");
+                }
+                else
+                {
+                    Console.WriteLine("Balita");
+                }
+            }
+
+            Console.ReadLine();
+        }*/
+    }
+}
+
