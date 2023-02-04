545d3245be971275979337053c08bffb434c30e3
romiramadani21 <romiramaadani@gmail.com>
2023-02-04 13:22:48 +0700

    Aritmatika in C#

---
+++ csharp/Aritmatika.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Aritmatika
+    {
+        /**static void Main(string[] args)
+        {
+            Console.Write("Tulis angka ke 1 : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Tulis angka ke 2 : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hasil Penjumlahan adalah {0}", x + y);
+            Console.WriteLine("Hasil Perkalian adalah {0}", x * y);
+            Console.WriteLine("Hasil Pembagian adalah {0}", Convert.ToDouble(x) / Convert.ToDouble(y));
+            Console.WriteLine("Hasil Pengurangan adalah {0}", x - y);
+            Console.WriteLine("Hasil Modulus adalah {0}", x % y);
+
+            Console.ReadLine();
+        }
+        */
+    }
+}
