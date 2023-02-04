2513c9c7b6915d3fb7517d98554353d42917208f
romiramadani21 <romiramaadani@gmail.com>
2023-02-04 13:06:54 +0700

    Add Variable in C#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,11 +8,11 @@ namespace csharp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World");
 
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
     }
 }
---
+++ csharp/Variable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Variable
+    {
+        static void Main(string[] args)
+        {
+            int x = 42; // Bilangan bulat
+            double phi = 3.14; // Bilangan desimal
+            char y = 'Z'; // Satu Character
+            string hai = "Hello"; // Terdiri dari beberapa character
+            bool kondisi = true; // True Or False
+
+            Console.WriteLine($"Nilai {x} \n" +
+                $"Nilai Phi {phi} \n" +
+                $"Nilai char {y} \n" +
+                $"Kata y {y} \n" +
+                $"Menyapa hai {hai} \n" +
+                $"Nilai Kondisi {kondisi} \n");
+            Console.ReadLine();
+        }
+    }
+}
