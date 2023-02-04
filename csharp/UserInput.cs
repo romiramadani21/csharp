b7cad920ca07aa20977a43b50eded2ab1595a022
romiramadani21 <romiramaadani@gmail.com>
2023-02-04 13:12:56 +0700

    Userinput in C#

---
+++ csharp/UserInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class UserInput
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Masukan tulisan anda : ");
+        //    string word = Console.ReadLine();
+        //    Console.WriteLine("Anda Menulis {0}", word);
+
+        //    Console.ReadLine();
+        //}
+    }
+}
--- csharp/Variable.cs
+++ csharp/Variable.cs
@@ -8,21 +8,21 @@ namespace csharp
 {
     class Variable
     {
-        static void Main(string[] args)
-        {
-            int x = 42; // Bilangan bulat
-            double phi = 3.14; // Bilangan desimal
-            char y = 'Z'; // Satu Character
-            string hai = "Hello"; // Terdiri dari beberapa character
-            bool kondisi = true; // True Or False
+        //static void Main(string[] args)
+        //{
+        //    int x = 42; // Bilangan bulat
+        //    double phi = 3.14; // Bilangan desimal
+        //    char y = 'Z'; // Satu Character
+        //    string hai = "Hello"; // Terdiri dari beberapa character
+        //    bool kondisi = true; // True Or False
 
-            Console.WriteLine($"Nilai {x} \n" +
-                $"Nilai Phi {phi} \n" +
-                $"Nilai char {y} \n" +
-                $"Kata y {y} \n" +
-                $"Menyapa hai {hai} \n" +
-                $"Nilai Kondisi {kondisi} \n");
-            Console.ReadLine();
-        }
+        //    Console.WriteLine($"Nilai {x} \n" +
+        //        $"Nilai Phi {phi} \n" +
+        //        $"Nilai char {y} \n" +
+        //        $"Kata y {y} \n" +
+        //        $"Menyapa hai {hai} \n" +
+        //        $"Nilai Kondisi {kondisi} \n");
+        //    Console.ReadLine();
+        //}
     }
 }
