289eaafebadf19ba5750d89d65466e219ca3b26a
romiramadani21 <romiramaadani@gmail.com>
2023-02-18 09:26:09 +0700

    add arguments in csharp

---
+++ csharp/Arguments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Arguments
+    {
+       /* static void Main(string[] args)
+        {
+            int a = 5;
+            Hitung(ref a);
+            int b = 7;
+            getNilai(out a, out b);
+
+            Console.WriteLine(a + " " + b);
+            Console.ReadLine();
+        }*/
+
+        static void getNilai(out int x, out int y)
+        {
+            x = 15;
+            y = 12;
+        }
+
+        static void Hitung(ref int x)
+        {
+            x = x * x;
+        }
+    }
+}
