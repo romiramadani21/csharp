9124fa36b0add4dd53a879f0e58363a25dc15a76
romiramadani21 <romiramaadani@gmail.com>
2023-02-18 09:05:37 +0700

    Add Looping in csharp

---
+++ csharp/Loop.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Loop
+    {
+       /* static void Main(string[] args)
+        {
+            int x = 1;
+            Console.WriteLine("Looping While");
+            while (++x < 6)
+                Console.WriteLine(x);
+
+            Console.WriteLine("Looping Do-While");
+            int y = 10;
+            do
+            {
+                Console.WriteLine(y);
+                y++;
+            } while (y < 15);
+
+
+            Console.WriteLine("Looping For");
+            int jumlah = 1;
+            for (int z = 1; z < 10; z++)
+            {
+                Console.WriteLine("Nilai x = " + z);
+                jumlah *= z;
+            }
+            Console.WriteLine("Total Penjumlahan nilai x adalah = " + jumlah);
+
+            Console.ReadLine();
+        }*/
+    }
+}
