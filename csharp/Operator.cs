57a986ed4e16d1f136d85a01558f5b7008eb0371
romiramadani21 <romiramaadani@gmail.com>
2023-02-18 09:09:19 +0700

    Add Operator in csharp

---
+++ csharp/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Operator
+    {
+       /* static void Main(string[] args)
+        {
+            int umur = 35;
+            double uang = 900;
+            bool kawin = false;
+
+            if (!(umur > 25) || (uang > 1000) && !kawin)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Failure");
+            }
+            Console.ReadLine();
+        }*/
+    }
+}
