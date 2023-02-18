f5b431da92c29705af389998136037130670b77a
romiramadani21 <romiramaadani@gmail.com>
2023-02-18 08:52:41 +0700

    Add Increment & Switchcase

---
+++ csharp/IncrementAndDecrement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class IncrementAndDecrement
+    {
+       /* static void Main(string[] args)
+        {
+            int x = 10;
+            x += 2;
+            int y = x++;
+
+            Console.WriteLine("Nilai Y adalah : {0}", y);
+
+            Console.ReadLine();
+        }*/
+    }
+}
---
+++ csharp/SwitchCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class SwitchCase
+    {
+        /*static void Main(string[] args)
+        {
+            int x = 8;
+
+            switch (x)
+            {
+                case 8:
+                    Console.WriteLine("Nilai x adalah {0}", x);
+                    break;
+                case 18:
+                    Console.WriteLine("Nilai x adalah {0}", x);
+                    break;
+                case 20:
+                    Console.WriteLine("Nilai x adalah {0}", x);
+                    break;
+                default:
+                    Console.WriteLine("Nilai x tidak ada yang cocok");
+                    break;
+            }
+            Console.ReadLine();
+        }*/
+    }
+}
