b808008a352b0b9d6c48b5dc735ac7d8194aa746
romiramadani21 <romiramaadani@gmail.com>
2023-02-18 09:36:14 +0700

    add array in csharp

---
+++ csharp/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Array
+    {
+        /*static void Main(string[] args)
+        {
+            int[] a = { 1, 3, 5, 7, 9 };
+            double[] b = { 1.4, 3.5, 6.7, 8.9, 0.9 };
+
+            for (int x = 0; x < b.Length; x++)
+            {
+                Console.Write(b[x] + " ");
+            }
+            Console.Write("= ");
+            Double jumlah = 0;
+
+            foreach (double x in b)
+            {
+                jumlah += x;
+            }
+            Console.Write(jumlah);
+            Console.ReadLine();
+        }*/
+    }
+}
